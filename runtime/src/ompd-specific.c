9ea80d821984cdcf7f578a97d1a14e5a2a131a41
Joachim Protze <protze1@llnl.gov>
2015-08-09 02:15:42 -0700

    Revert "calculate offset and size information at compile time" Issue with this change: const variables are optimized out and not available in the binary.
    This reverts commit b89b9e0b466abe5a53a2815fcc030a9c13f5d1f3.

--- runtime/src/ompd-specific.c
+++ runtime/src/ompd-specific.c
@@ -6,7 +6,7 @@
  * Declaration of symbols to hold struct size and member offset information
  */
 
-#define ompd_declare_access(t,m) const uint64_t ompd_access__##t##__##m = (uint64_t)&(((t*)0)->m); 
+#define ompd_declare_access(t,m) uint64_t ompd_access__##t##__##m; 
 OMPD_FOREACH_ACCESS(ompd_declare_access)
 #undef ompd_declare_access
 
@@ -14,7 +14,7 @@ OMPD_FOREACH_ACCESS(ompd_declare_access)
 OMPD_FOREACH_BITFIELD(ompd_declare_bitfield)
 #undef ompd_declare_bitfield
 
-#define ompd_declare_sizeof(t) const uint64_t ompd_sizeof__##t = sizeof(t); 
+#define ompd_declare_sizeof(t) uint64_t ompd_sizeof__##t; 
 OMPD_FOREACH_SIZEOF(ompd_declare_sizeof)
 #undef ompd_declare_sizeof
 
@@ -33,12 +33,28 @@ if (ompd_initialized)
   return;
   
 /**
+ * Calculate member offsets for structs and unions
+ */
+
+#define ompd_init_access(t,m) ompd_access__##t##__##m = (uint64_t)&(((t*)0)->m); 
+OMPD_FOREACH_ACCESS(ompd_init_access)
+#undef ompd_init_access
+
+/**
  * Create bit mask for bitfield access
  */
 
 #define ompd_init_bitfield(t,m) ompd_bitfield__##t##__##m=0; ((t*)(&ompd_bitfield__##t##__##m))->m = -1; 
 OMPD_FOREACH_BITFIELD(ompd_init_bitfield)
 #undef ompd_init_bitfield
+
+/**
+ * Calculate type size information
+ */
+
+#define ompd_init_sizeof(t) ompd_sizeof__##t = sizeof(t); 
+OMPD_FOREACH_SIZEOF(ompd_init_sizeof)
+#undef ompd_init_sizeof
 
   ompd_dll_locations=ompd_my_dll_locations;
   ompd_dll_locations_valid ();
