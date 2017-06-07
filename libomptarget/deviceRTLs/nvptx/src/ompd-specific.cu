4d937523a41acd4f5e2f9f0a9b16878740db9910
Marty McFadden <mcfadden8@llnl.gov>
2017-06-07 13:51:39 -0700

    Checkpoint - CUDA target support for OMPD in progress

--- libomptarget/deviceRTLs/nvptx/src/ompd-specific.cu
+++ libomptarget/deviceRTLs/nvptx/src/ompd-specific.cu
@@ -5,52 +5,60 @@
  * Declaration of symbols to hold struct size and member offset information
  */
 
-__device__ __shared__ static int ompd_target_initialized;
-__device__ __shared__ uint64_t ompd_target_access__omptarget_nvptx_TaskDesc__data_items_threadId;
-__device__ __shared__ uint64_t ompd_target_sizeof__omptarget_nvptx_TaskDesc__data_items_threadId;
+__device__ __shared__ static int ompd_initialized;
 
-#define ompd_target_declare_access(t,m) __device__ __shared__ uint64_t ompd_target_access__##t##__##m; 
-OMPD_FOREACH_ACCESS(ompd_target_declare_access)
-#undef ompd_target_declare_access
+#define ompd_declare_access(t,m1,m2,m3) __device__ __shared__ uint64_t ompd_access__##t##__##m1##__##m2##__##m3; 
+OMPD_FOREACH_ACCESS3(ompd_declare_access)
+#undef ompd_declare_access
 
-#define ompd_target_declare_sizeof_member(t,m) __device__ __shared__ uint64_t ompd_target_sizeof__##t##__##m; 
-OMPD_FOREACH_ACCESS(ompd_target_declare_sizeof_member)
-#undef ompd_target_declare_sizeof_member
+#define ompd_declare_access(t,m) __device__ __shared__ uint64_t ompd_access__##t##__##m; 
+OMPD_FOREACH_ACCESS(ompd_declare_access)
+#undef ompd_declare_access
 
-#define ompd_target_declare_sizeof(t) __device__ __shared__ uint64_t ompd_target_sizeof__##t; 
-OMPD_FOREACH_SIZEOF(ompd_target_declare_sizeof)
-#undef ompd_target_declare_sizeof
+#define ompd_declare_sizeof_member(t,m) __device__ __shared__ uint64_t ompd_sizeof__##t##__##m; 
+OMPD_FOREACH_ACCESS(ompd_declare_sizeof_member)
+#undef ompd_declare_sizeof_member
+
+#define ompd_declare_sizeof_member(t,m1,m2,m3) __device__ __shared__ uint64_t ompd_sizeof__##t##__##m1##__##m2##__##m3; 
+OMPD_FOREACH_ACCESS3(ompd_declare_sizeof_member)
+#undef ompd_declare_sizeof_member
+
+#define ompd_declare_sizeof(t) __device__ __shared__ uint64_t ompd_sizeof__##t; 
+OMPD_FOREACH_SIZEOF(ompd_declare_sizeof)
+#undef ompd_declare_sizeof
 
 __device__ void ompd_init ( void )
 {
-if (ompd_target_initialized)
+if (ompd_initialized)
   return;
   
 /**
  * Calculate member offsets for structs and unions
  */
-ompd_target_access__omptarget_nvptx_TaskDesc__data_items_threadId = 
-          (uint64_t)&(((omptarget_nvptx_TaskDescr*)0)->data.items.threadId);
+#define ompd_init_access(t,m) ompd_access__##t##__##m = (uint64_t)&(((t*)0)->m); 
+OMPD_FOREACH_ACCESS(ompd_init_access)
+#undef ompd_init_access
 
-#define ompd_target_init_access(t,m) ompd_target_access__##t##__##m = (uint64_t)&(((t*)0)->m); 
-OMPD_FOREACH_ACCESS(ompd_target_init_access)
-#undef ompd_target_init_access
+#define ompd_init_access(t,m1,m2,m3) ompd_access__##t##__##m1##__##m2##__##m3 = (uint64_t)&(((t*)0)->m1.m2.m3); 
+OMPD_FOREACH_ACCESS3(ompd_init_access)
+#undef ompd_init_access
 
 /**
  * Calculate type size information
  */
-ompd_target_sizeof__omptarget_nvptx_TaskDesc__data_items_threadId = 
-          (uint64_t)sizeof(((omptarget_nvptx_TaskDescr*)0)->data.items.threadId);
+#define ompd_init_sizeof_member(t,m) ompd_sizeof__##t##__##m = sizeof(((t*)0)->m); 
+OMPD_FOREACH_ACCESS(ompd_init_sizeof_member)
+#undef ompd_init_sizeof
 
-#define ompd_target_init_sizeof_member(t,m) ompd_target_sizeof__##t##__##m = sizeof(((t*)0)->m); 
-OMPD_FOREACH_ACCESS(ompd_target_init_sizeof_member)
-#undef ompd_target_init_sizeof_member
+#define ompd_init_sizeof(t,m1,m2,m3) ompd_sizeof__##t##__##m1##__##m2##__##m3 = sizeof(((t*)0)->m1.m2.m3); 
+OMPD_FOREACH_ACCESS3(ompd_init_sizeof)
+#undef ompd_init_sizeof
 
-#define ompd_target_init_sizeof(t) ompd_target_sizeof__##t = sizeof(t); 
-OMPD_FOREACH_SIZEOF(ompd_target_init_sizeof)
-#undef ompd_target_init_sizeof
+#define ompd_init_sizeof(t) ompd_sizeof__##t = sizeof(t); 
+OMPD_FOREACH_SIZEOF(ompd_init_sizeof)
+#undef ompd_init_sizeof
 
-  ompd_target_initialized = 1;
+  ompd_initialized = 1;
 }
 
 __device__ void ompd_bp_parallel_begin (){ asm (""); }
--- libomptarget/deviceRTLs/nvptx/src/ompd-specific.h
+++ libomptarget/deviceRTLs/nvptx/src/ompd-specific.h
@@ -1,4 +1,4 @@
-// TODO(Marty) - Need to make OMPD_SUPPORT configurable in CMAKE
+// TODO(mjm) - Need to make OMPD_SUPPORT configurable in CMAKE
 #define OMPD_SUPPORT 1
 #ifdef OMPD_SUPPORT
 
@@ -10,8 +10,8 @@
 #ifndef __OMPD_SPECIFIC_H__
 #define __OMPD_SPECIFIC_H__
 
-
 __device__ void ompd_init( void );
+
 extern "C" __device__ void ompd_bp_parallel_begin ( void );
 extern "C" __device__ void ompd_bp_parallel_end ( void );
 extern "C" __device__ void ompd_bp_task_begin ( void );
@@ -19,6 +19,9 @@ extern "C" __device__ void ompd_bp_task_end ( void );
 
 #define OMPD_FOREACH_ACCESS(OMPD_ACCESS) \
 OMPD_ACCESS(omptarget_nvptx_ThreadPrivateContext, topTaskDescr)
+
+#define OMPD_FOREACH_ACCESS3(OMPD_ACCESS) \
+OMPD_ACCESS(omptarget_nvptx_TaskDescr, data, items, threadId)
 
 #define OMPD_FOREACH_SIZEOF(OMPD_SIZEOF) \
 OMPD_SIZEOF(omptarget_nvptx_ThreadPrivateContext)\
